645aa99ea9d932d4a2f48c5f4655d1426ab8a212
Carl Åstholm <carl@astholm.se>
2025-02-11 17:20:23 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+<!--
+© 2025 Carl Åstholm
+SPDX-License-Identifier: MIT
+-->
+
+# printfcop 🖨️👮‍♂️
+
+printfcop
---
+++ build.zig
@@ -0,0 +1,8 @@
+// © 2025 Carl Åstholm
+// SPDX-License-Identifier: MIT
+
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    _ = b;
+}
